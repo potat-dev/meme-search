d168ebb7352211e9d91542019b78b806ed879645
Den <denis100703@gmail.com>
2022-07-27 23:26:52 +0300

    caching data with pickle for dev

--- async_get_posts.py
+++ async_get_posts.py
@@ -1,6 +1,8 @@
 from mpire import WorkerPool
 from vk_api import VkApi
 from math import ceil
+import pickle
+import json
 import re
 
 with open('token.txt', 'r') as tokens:
@@ -8,6 +10,7 @@ with open('token.txt', 'r') as tokens:
     vk = VkApi(token=token)
 
 pub_name = 'poiskmemow'
+# pub_name = 'kartinochkistekstom'
 max_items = 100
 
 def format_text(text):
@@ -49,6 +52,14 @@ class Post:
   def __str__(self):
     return f'Post(id={self.id}, text={self.text}, photos={self.photos})'
 
+  # to dict
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'text': self.text,
+      'photos': self.photos
+    }
+
 
 if __name__ == '__main__':
     count = vk.method("wall.get", {'domain': pub_name, 'count': 1})['count']
@@ -65,58 +76,14 @@ if __name__ == '__main__':
     print("all posts:", len(posts))
 
     posts = [post for post in posts if post.photos and post.text]
-    print("without images:", len(posts))
+    print("with images:", len(posts))
 
     with open('only_text.txt', 'w', encoding="utf-8") as f:
       for post in posts:
         f.write(f'{post.text}\n')
 
+    with open('posts_data.pickle', 'wb') as f:
+      pickle.dump([post.to_dict() for post in posts], f)
 
-
-
-
-# api_endpoint = 'https://api.vk.com/method/wall.get'
-# pub_name = 'poiskmemow'
-# max_items = 100
-
-# with open('token.txt', 'r') as tokens:
-#     access_token = tokens.readline()
-
-# posts_data = []
-
-
-# async def get_page_data(session, page):
-#     global posts_data
-#     posts_url = f'{api_endpoint}?domain={pub_name}&count={max_items}&offset={page * max_items}&access_token={access_token}&v=5.131'
-#     async with session.get(posts_url) as responce:
-#         data = await responce.json()
-#         posts_data += data['response']['items']
-#     print(f'Получена страница {page}')
-
-
-# async def gather_data():
-#     posts_count_url = f'{api_endpoint}?domain={pub_name}&count={1}&access_token={access_token}&v=5.131'
-#     async with aiohttp.ClientSession() as session:
-#         responce = await session.get(posts_count_url)
-#         data = await responce.json()
-#         posts_count = data['response']['count']
-#         api_calls_count = ceil(posts_count / max_items)
-#         print(posts_count, api_calls_count)
-
-#         tasks = []
-#         for page in range(api_calls_count):
-#             task = asyncio.create_task(get_page_data(session, page))
-#             tasks.append(task)
-
-#         await asyncio.gather(*tasks)
-
-
-# def main():
-#     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-#     start_time = time()
-#     asyncio.run(gather_data())
-#     finish_time = time() - start_time
-#     print(f'Время выполнения: {finish_time}')
-#     print(f'Количество постов: {len(posts_data)}')
-
-# main()
+    with open('posts_data.json', 'w', encoding="utf-8") as f:
+      f.write(json.dumps([{"keywords": post.text, "images": post.photos} for post in posts], ensure_ascii=False))
