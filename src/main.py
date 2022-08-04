20454e1159678ff906bc369daaae7587f73702ec
Den <denis100703@gmail.com>
2022-08-04 23:18:32 +0300

    CORS error fixed

--- src/main.py
+++ src/main.py
@@ -1,35 +1,53 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fuzzywuzzy import process, utils, fuzz
 from Database import database
 from settings import settings
+from time import sleep
+from halo import Halo
 
 db = database.Database(settings)
 
 
 def update_database():
-    print('Updating database...')  # TODO: change to logger
-    keywords, images = db.get_keywords(), db.get_images()
-    print('Database updated ✅')
-    return keywords, images
+    with Halo(text='Updating Database', spinner='bounce') as spinner:
+        try:
+            keywords, images = (db.get_keywords(), db.get_images())
+            spinner.succeed("Database Updated!")
+            return (keywords, images)
+        except Exception as e:
+            spinner.fail("Something went wrong!")
+            print(e)
+            return (None, None)
 
 
 keywords, images = update_database()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/search/{query}")
-async def search(query: str, limit: int = 10, min_score: int = 70):
+@app.get("/search")
+async def search(q: str, limit: int = 10, min_score: int = 70, use_delay: bool = False):
+    if use_delay:
+        sleep(5)  # for testing purposes (react loading UI)
     results = process.extract(
         choices=keywords,
-        query=query,
+        query=q,
         limit=limit,
         scorer=fuzz.partial_ratio
-    )
+    )  # -> list of tuples (keywords, score, id)
     results = [
         {
             "text": result[0],
             "score": result[1],
-            "id": result[2],
+            "id": str(result[2]),
             "images": images[result[2]]
         }
         for result in results if result[1] >= min_score
