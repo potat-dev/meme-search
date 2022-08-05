ec47d611b034f54068abb4f1437d0ec040a95816
Den <denis100703@gmail.com>
2022-08-05 23:54:32 +0300

    Caching method improved & faster update()

--- src/Database/database.py
+++ src/Database/database.py
@@ -1,17 +1,71 @@
 from pymongo import MongoClient
+from dataclasses import dataclass
+from halo import Halo
+
 
 class Database:
-  def __init__(self, settings):
-    self.client = MongoClient(settings.mongo_uri)
-    self.db = self.client[settings.mongo_db]
-    self.collection = self.db[settings.mongo_collection]
+    """
+    Wrapper for mongoDB find() method
+    """
+    def __init__(self, settings):
+        self.client = MongoClient(settings.mongo_uri)
+        self.db = self.client[settings.mongo_db]
+        self.collection = self.db[settings.mongo_collection]
 
-  def get_keywords(self):
-    data = self.collection.find(projection=['keywords'])
-    keywords = {i['_id']: i['keywords'] for i in data}
-    return keywords
+    def get_ids(self):
+        data = self.collection.find(projection=['_id'])
+        return set(i['_id'] for i in data)
 
-  def get_images(self):
-    data = self.collection.find(projection=['images'])
-    images = {i['_id']: i['images'] for i in data}
-    return images
+    def get_posts(self, ids: list):
+        return self.collection.find({'_id': {'$in': ids}})
+
+
+@dataclass
+class Cache:
+    """
+    Local cache of keywords and images
+    for faster access and quick fuzzy search
+    """
+    db: Database
+    keywords = {}
+    images = {}
+
+    @property
+    def ids(self):
+        return set(i for i, _ in self.keywords.items())
+
+    @property
+    def posts_count(self):
+        return len(self.keywords)
+
+    def add_posts(self, posts):
+        self.keywords.update({p['_id']: p['keywords'] for p in posts})
+        self.images.update({p['_id']: p['images'] for p in posts})
+
+    def remove_posts(self, ids):
+        for id in ids:
+            self.keywords.pop(id, None)
+            self.images.pop(id, None)
+
+    def update(self):
+        with Halo(text='Updating cache', spinner='bounce') as spinner:
+            cached_ids = self.ids
+            try:
+                server_ids = self.db.get_ids()
+            except Exception as e:
+                spinner.fail("Something went wrong!")
+                print("Exception:", e)
+                return None
+
+            new_ids = server_ids - cached_ids
+            if len(new_ids) > 0:
+                new_posts = self.db.get_posts(list(new_ids))
+                self.add_posts(new_posts)
+
+            removed_ids = cached_ids - server_ids
+            self.remove_posts(removed_ids)
+
+            if len(new_ids) > 0 or len(removed_ids) > 0:
+                spinner.succeed(f"Cache Updated! ({len(new_ids)} added {len(removed_ids)} removed)")
+            else:
+                spinner.warn("Cache is up to date!")
--- src/main.py
+++ src/main.py
@@ -1,27 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fuzzywuzzy import process, utils, fuzz
-from Database import database
-from settings import settings
+from fuzzywuzzy import process, fuzz
 from time import sleep
-from halo import Halo
-
-db = database.Database(settings)
-
-
-def update_database():
-    with Halo(text='Updating Database', spinner='bounce') as spinner:
-        try:
-            keywords, images = (db.get_keywords(), db.get_images())
-            spinner.succeed("Database Updated!")
-            return (keywords, images)
-        except Exception as e:
-            spinner.fail("Something went wrong!")
-            print(e)
-            return (None, None)
+from settings import settings
+from Database.database import Database, Cache
 
 
-keywords, images = update_database()
+db = Database(settings)
+cache = Cache(db)
+cache.update()
 
 app = FastAPI()
 app.add_middleware(
@@ -38,7 +25,7 @@ async def search(q: str, limit: int = 10, min_score: int = 70, use_delay: bool =
     if use_delay:
         sleep(5)  # for testing purposes (react loading UI)
     results = process.extract(
-        choices=keywords,
+        choices=cache.keywords,
         query=q,
         limit=limit,
         scorer=fuzz.partial_ratio
@@ -48,7 +35,7 @@ async def search(q: str, limit: int = 10, min_score: int = 70, use_delay: bool =
             "text": result[0],
             "score": result[1],
             "id": str(result[2]),
-            "images": images[result[2]]
+            "images": cache.images[result[2]]
         }
         for result in results if result[1] >= min_score
     ]
@@ -63,22 +50,21 @@ async def search(q: str, limit: int = 10, min_score: int = 70, use_delay: bool =
 async def meme(id: int):
     return {
         "id": id,
-        "keywords": keywords[id],
-        "images": images[id]
+        "keywords": cache.keywords[id],
+        "images": cache.images[id]
     }
 
 
 @app.get("/stats")
 async def stats():
     return {
-        "count": len(keywords),
+        "count": cache.posts_count,
     }
 
 
 @app.post("/update")
 async def update():
-    global keywords, images
-    keywords, images = update_database()
+    cache.update()
     return {
-        "count": len(keywords),
+        "count": cache.posts_count,
     }
